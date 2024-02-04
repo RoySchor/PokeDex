643140e123d19d35aa0d8897bc1a88cfa77739fd
Roy Schor <rps5836@psu.edu>
2024-02-03 20:42:54 -0500

    initial commit for new app project - pentomino puzzle

---
+++ Pentomino Puzzle/Pentomino Puzzle/Pentomino_PuzzleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pentomino_PuzzleApp.swift
+//  Pentomino Puzzle
+//
+//  Created by Roy Schor on 2/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Pentomino_PuzzleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
