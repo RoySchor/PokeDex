2018a59a4667e7536df63fb16202ed31181bd3c1
Roy Schor <rps5836@psu.edu>
2024-02-19 17:08:54 -0500

    initial commit for campusMap project

---
+++ CampusMap/CampusMap/CampusMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  CampusMapApp.swift
+//  CampusMap
+//
+//  Created by Roy Schor on 2/19/24.
+//
+
+import SwiftUI
+
+@main
+struct CampusMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
