e6649a8a90044b26873063a31e6f04a1a815529b
Roy Schor <rps5836@psu.edu>
2024-03-18 00:24:42 -0400

    initial project commit

---
+++ Pokedex/Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Roy Schor on 3/18/24.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
