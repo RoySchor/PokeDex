7ed9b02486c179fd39418d463db9652116889e08
Roy Schor <rps5836@psu.edu>
2024-03-25 19:36:16 -0400

    initial commit for final project - poker app

---
+++ PokerPro/PokerPro/PokerProApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokerProApp.swift
+//  PokerPro
+//
+//  Created by Roy Schor on 3/25/24.
+//
+
+import SwiftUI
+
+@main
+struct PokerProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
